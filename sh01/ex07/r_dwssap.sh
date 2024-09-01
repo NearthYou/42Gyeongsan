8ceecf1203931ced35056566ada0b05add745636
silee2 <silee2@student.42gyeongsan.kr>
2024-09-02 02:10:44 +0900

    remove files

--- sh01/ex07/r_dwssap.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat /etc/passwd | sed '/^#/d'| sed -n 'n;p' | cut -d ":" -f1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g'
--- sh01/ex08/add_chelou.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-x=$(echo $FT_NBR1 | tr "'" "0" | sed -r 's"[\]"1"g' | tr '"' '2' | tr "?" "3" | tr "!" "4")
-y=$(echo $FT_NBR2 | tr "mrdoc" "01234")
-echo $x+$y | xargs echo "obase=13; ibase=5;" | bc | tr "0123456789ABC" "gtaio luSnemf"
